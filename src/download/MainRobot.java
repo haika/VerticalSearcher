0c320fe66ad98b3f73c9c4d10805053964309061
panjf2000 <panjf2000@gmail.com>
2015-03-29 22:03:41 +0800

    在main()函数中使用Callable+Future来等待Download子线程完成工作后启动Index类进行索引，使程序更加自动化

--- src/download/MainRobot.java
+++ src/download/MainRobot.java
@@ -1,27 +1,67 @@
 package download;
+import index.Index;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.io.IOException;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 import utils.MySQL;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 public class MainRobot {
 
 	private MySQL db = new MySQL();
 	private String startUrl = "";
 	private MaintainLinks maintainQueue = new MaintainLinks();
-
-	public static void main(String[] args) {
+	
+	public static void main(String[] args) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, InstantiationException {
 		String start = "http://www.zol.com.cn/";
 		new MainRobot(start);
+		ExecutorService executor = Executors.newCachedThreadPool();
+		Future<?>[] futures=new Future<?>[10];
+		for(int i=0;i<10;i++)
+		{
+			futures[i]=executor.submit(new Download());
+			try {
+				Thread.sleep(3000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		executor.shutdown();
+		for(Future<?> future:futures)
+			try {
+				future.get();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ExecutionException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		try {
+			Method method=Index.class.getMethod("main", String[].class);
+			method.invoke(null,new Object[]{null});
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public MainRobot(String start) {
 		this.startUrl = start;
 		try {
 			downloadFirstPage(startUrl);
-			new Download().start();
+			/*new Download().start();
 			Thread.sleep(3000);
 			new Download().start();
 			Thread.sleep(3000);
@@ -33,13 +73,14 @@ public class MainRobot {
 			Thread.sleep(3000);
 			new Download().start();
 			Thread.sleep(3000);
-			new Download().start();
+			new Download().start();*/
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 			System.err.println(startUrl + "下载失败");
-		} catch (InterruptedException e) {
+		} /*catch (InterruptedException e) {
 			e.printStackTrace();
-		}
+		}*/
 
 	}
 
